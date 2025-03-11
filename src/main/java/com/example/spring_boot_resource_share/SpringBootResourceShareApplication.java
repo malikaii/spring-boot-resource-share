56a92252ed6aaf011e04eccb9038e7284a9044b0
Malick Ouedraogo <C00400598@louisiana.edu>
2025-03-11 09:06:24 -0500

    first commit

---
+++ src/main/java/com/example/spring_boot_resource_share/SpringBootResourceShareApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_resource_share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootResourceShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootResourceShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_resource_share/SpringBootResourceShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_resource_share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootResourceShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
